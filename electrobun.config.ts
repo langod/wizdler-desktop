7a29942e364ed32e1028497302e233952bf55def
langod <langodik@gmail.com>
2026-07-13 21:58:02 +0200

    change app name

--- electrobun.config.ts
+++ electrobun.config.ts
@@ -2,8 +2,8 @@ import type { ElectrobunConfig } from "electrobun";
 
 export default {
 	app: {
-		name: "react-tailwind-vite",
-		identifier: "reacttailwindvite.electrobun.dev",
+		name: "wizdler-desktop",
+		identifier: "wizdler.desktop",
 		version: "0.0.1",
 	},
 	build: {
